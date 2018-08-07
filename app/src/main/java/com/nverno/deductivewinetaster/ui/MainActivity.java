b072b623fbb1dbe49afd8c99939c396bc9332c50
Cory <chanson5000@gmail.com>
2018-08-07 15:39:42 -0500

    Added onClick handlers for red and white wine buttons. Added intent for red wine form activity.

--- app/src/main/java/com/nverno/deductivewinetaster/ui/MainActivity.java
+++ app/src/main/java/com/nverno/deductivewinetaster/ui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.nverno.deductivewinetaster.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.nverno.deductivewinetaster.R;
+import com.nverno.deductivewinetaster.util.IntentContract;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonRedWine(View view) {
+        Intent intent = new Intent(this, RedDeductionFormActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonWhiteWine(View view) {
+        Intent intent = new Intent(this, RedDeductionFormActivity.class);
+        intent.putExtra(IntentContract.IS_RED_WINE, false);
     }
 }
