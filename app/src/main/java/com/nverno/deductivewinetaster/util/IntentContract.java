b4c8127305481f1a185aab24366d8040a15b953c
Cory <chanson5000@gmail.com>
2018-08-08 16:03:18 -0500

    Changed to RedWineContract for shareable fields.

--- app/src/main/java/com/nverno/deductivewinetaster/util/IntentContract.java
+++
@@ -1,5 +0,0 @@
-package com.nverno.deductivewinetaster.util;
-
-public interface IntentContract {
-    String IS_RED_WINE = "IS_RED_WINE";
-}
---
+++ app/src/main/java/com/nverno/deductivewinetaster/util/RedWineContract.java
@@ -0,0 +1,5 @@
+package com.nverno.deductivewinetaster.util;
+
+public interface RedWineContract {
+    String CURRENT_PAGE = "CURRENT_PAGE";
+}
