b56394ed3d3f8f622e7d4c68692eafd13cf44462
Cory <chanson5000@gmail.com>
2018-08-16 20:23:52 -0500

    Weird spacing fix.

--- app/src/main/java/com/nverno/deductivewinetaster/ui/ActualWineActivity.java
+++ app/src/main/java/com/nverno/deductivewinetaster/ui/ActualWineActivity.java
@@ -12,6 +12,5 @@ public class ActualWineActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actual_wine);
-        
     }
 }
