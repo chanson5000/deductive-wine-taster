271ddd46069f229de108fe4f2948d16e6a9fcacd
Cory <chanson5000@gmail.com>
2018-08-16 20:21:58 -0500

    Added first unit test for wine class.

--- app/src/test/java/com/nverno/deductivewinetaster/WineClassUnitTests.java
+++ app/src/test/java/com/nverno/deductivewinetaster/WineClassUnitTests.java
@@ -1,5 +1,7 @@
 package com.nverno.deductivewinetaster;
 
+import com.nverno.deductivewinetaster.model.RedWine;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,9 +11,21 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class WineClassUnitTests {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void redWine_clarityShouldMatch() {
+        RedWine redWine = new RedWine();
+
+        int expected = 1;
+        redWine.clarity().setClear(expected);
+
+        int result = redWine.clarity().getClear();
+
+        assertEquals(expected, result);
     }
 }
