727f1921593605406d14aa3d96bec7330d360697
Cory <chanson5000@gmail.com>
2018-08-17 14:55:23 -0500

    Made this runnable for now.

--- app/src/main/java/com/nverno/deductivewinetaster/data/FirebaseDataContract.java
+++ app/src/main/java/com/nverno/deductivewinetaster/data/FirebaseDataContract.java
@@ -1,8 +1,6 @@
 package com.nverno.deductivewinetaster.data;
 
 import com.nverno.deductivewinetaster.R;
-import com.nverno.deductivewinetaster.ui.DeductionFormContract;
-import com.nverno.deductivewinetaster.ui.MainActivity;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,10 +12,6 @@ public interface FirebaseDataContract  {
     Map<Integer, String> propertiesMap = new HashMap<Integer, String>()
     {{
         put(R.id.radio_group_palate_acid, "acidity");
-    }};
-
-    Map<Integer, String> acidityMap = new HashMap<Integer, String>(){{
-        put(R.id.radio_palate_acid_low, "")
     }};
 
 }
