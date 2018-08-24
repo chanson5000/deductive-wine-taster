a0c6efb215a9043d09e5c1958147058f232903b9
Cory <chanson5000@gmail.com>
2018-08-23 23:09:59 -0500

    Planted the default Timber debug tree.

--- app/src/main/java/com/wineguesser/deductive/DeductiveWineTaster.java
+++ app/src/main/java/com/wineguesser/deductive/DeductiveWineTaster.java
@@ -4,10 +4,17 @@ import android.app.Application;
 
 import com.google.firebase.database.FirebaseDatabase;
 
+import timber.log.Timber;
+
 public class DeductiveWineTaster extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
