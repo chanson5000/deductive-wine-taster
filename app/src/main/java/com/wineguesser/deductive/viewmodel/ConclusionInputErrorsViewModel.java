4fced5e93e70386af63fd2b120314b95301b5866
Cory <chanson5000@gmail.com>
2018-09-05 16:20:09 -0500

    Added view model for errors on forms.

---
+++ app/src/main/java/com/wineguesser/deductive/viewmodel/ConclusionInputErrorsViewModel.java
@@ -0,0 +1,53 @@
+package com.wineguesser.deductive.viewmodel;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class ConclusionInputErrorsViewModel extends ViewModel {
+
+    private MutableLiveData<String> errorVariety = new MutableLiveData<>();
+    private MutableLiveData<String> errorCountry = new MutableLiveData<>();
+    private MutableLiveData<String> errorRegion = new MutableLiveData<>();
+    private MutableLiveData<String> errorQuality = new MutableLiveData<>();
+    private MutableLiveData<String> errorVintage = new MutableLiveData<>();
+
+    public MutableLiveData<String> getErrorVariety() {
+        return errorVariety;
+    }
+
+    public void setErrorVariety(String errorVariety) {
+        this.errorVariety.setValue(errorVariety);
+    }
+
+    public MutableLiveData<String> getErrorCountry() {
+        return errorCountry;
+    }
+
+    public void setErrorCountry(String errorCountry) {
+        this.errorCountry.setValue(errorCountry);
+    }
+
+    public MutableLiveData<String> getErrorRegion() {
+        return errorRegion;
+    }
+
+    public void setErrorRegion(String errorRegion) {
+        this.errorRegion.setValue(errorRegion);
+    }
+
+    public MutableLiveData<String> getErrorQuality() {
+        return errorQuality;
+    }
+
+    public void setErrorQuality(String errorQuality) {
+        this.errorQuality.setValue(errorQuality);
+    }
+
+    public MutableLiveData<String> getErrorVintage() {
+        return errorVintage;
+    }
+
+    public void setErrorVintage(String errorVintage) {
+        this.errorVintage.setValue(errorVintage);
+    }
+}
