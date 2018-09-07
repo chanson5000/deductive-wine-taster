7b0cfe737d2aa33d866304a808da205c08a6e239
Cory <chanson5000@gmail.com>
2018-09-07 16:07:40 -0500

    Added some tests, one is not working.

--- app/src/androidTest/java/com/wineguesser/deductive/DeductionFormActivityTests.java
+++ app/src/androidTest/java/com/wineguesser/deductive/DeductionFormActivityTests.java
@@ -1,32 +1,54 @@
 package com.wineguesser.deductive;
 
 import android.content.Intent;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.wineguesser.deductive.ui.DeductionFormActivity;
 import com.wineguesser.deductive.ui.DeductionFormContract;
+import com.wineguesser.deductive.ui.VarietyResultsActivity;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.pressBack;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.BundleMatchers.hasEntry;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtras;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class DeductionFormActivityTests implements DeductionFormContract {
 
     @Rule
-    public ActivityTestRule<DeductionFormActivity> activityTestRule =
-            new ActivityTestRule<>(DeductionFormActivity.class, false, false);
+    public IntentsTestRule<DeductionFormActivity> activityTestRule =
+            new IntentsTestRule<>(DeductionFormActivity.class, false, false);
 
     @Test
     public void onInputFinalConclusion_LoadingIndicatorShows() {
@@ -34,11 +56,132 @@ public class DeductionFormActivityTests implements DeductionFormContract {
         intent.putExtra(IS_RED_WINE, RED_WINE);
         activityTestRule.launchActivity(intent);
 
-        onView(withId(R.layout.fragment_sight_red)).perform(swipeRight());
-        onView(withId(R.layout.fragment_nose_red)).perform(swipeRight());
-        onView(withId(R.layout.fragment_palate_red_a)).perform(swipeRight());
-        onView(withId(R.layout.fragment_palate_red_b)).perform(swipeRight());
-        onView(withId(R.layout.fragment_initial_conclusion)).perform(swipeRight());
-        onView(withId(R.layout.fragment_final_conclusion))
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Clear Form")).perform(click());
+
+        // Sight Fragment
+        onView(withId(R.id.radio_color_ruby)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Nose Fragment
+        onView(withId(R.id.check_nose_fruit_black)).perform(scrollTo(), click());
+        onView(withId(R.id.check_nose_fruit_blue)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Palate Fragment A
+        onView(withId(R.id.check_palate_fruit_black)).perform(scrollTo(), click());
+        onView(withId(R.id.check_palate_fruit_blue)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Palate Fragment B
+        onView(withId(R.id.radio_palate_tannin_med_plus)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_acid_med_plus)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_body_full)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_texture_round)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_complexity_med_plus)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Initial Conclusion
+        onView(withId(R.id.multiText_initial_varieties)).perform(scrollTo(), replaceText("Pinot Noir"));
+        onView(withId(R.id.radio_button_initial_world_new)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_button_climate_warm)).perform(scrollTo(), click());
+        onView(withId(R.id.multiText_initial_countries)).perform(scrollTo(), replaceText("United States"));
+        onView(withId(R.id.radio_button_initial_age_onetothree)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Final Conclusion
+        onView(withId(R.id.autoText_final_grape_variety))
+                .perform(scrollTo(), replaceText("Pinot Noir"));
+        onView(withId(R.id.autoText_final_country))
+                .perform(scrollTo(), replaceText("United States"));
+        onView(withId(R.id.autoText_final_region))
+                .perform(scrollTo(), replaceText("California"));
+        onView(withId(R.id.autoText_final_quality))
+                .perform(scrollTo(), replaceText("None"));
+        onView(withId(R.id.autoText_final_vintage))
+                .perform(scrollTo(), replaceText("2012"));
+        onView(withId(R.id.button_submit_final_conclusion))
+                .perform(click());
+
+        onView(withId(R.id.progressBar_final_conclusion)).check(matches(not(isDisplayed())));
     }
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        Intents.init();
+//    }
+
+    @Test
+    @LargeTest
+    public void onInputFinalConclusion_WineIsGuessed() {
+        Intent intent = new Intent();
+        intent.putExtra(IS_RED_WINE, RED_WINE);
+        activityTestRule.launchActivity(intent);
+
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Clear Form")).perform(click());
+
+        // Sight Fragment
+        onView(withId(R.id.radio_color_ruby)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Nose Fragment
+        onView(withId(R.id.check_nose_fruit_black)).perform(scrollTo(), click());
+        onView(withId(R.id.check_nose_fruit_blue)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Palate Fragment A
+        onView(withId(R.id.check_palate_fruit_black)).perform(scrollTo(), click());
+        onView(withId(R.id.check_palate_fruit_blue)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Palate Fragment B
+        onView(withId(R.id.radio_palate_tannin_med_plus)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_acid_med_plus)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_body_full)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_texture_round)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_palate_complexity_med_plus)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Initial Conclusion
+        onView(withId(R.id.multiText_initial_varieties)).perform(scrollTo(), replaceText("Cabernet Sauvignon"));
+        onView(withId(R.id.radio_button_initial_world_new)).perform(scrollTo(), click());
+        onView(withId(R.id.radio_button_climate_warm)).perform(scrollTo(), click());
+        onView(withId(R.id.multiText_initial_countries)).perform(scrollTo(), replaceText("United States"));
+        onView(withId(R.id.radio_button_initial_age_onetothree)).perform(scrollTo(), click());
+        onView(withId(R.id.view_pager_red_deduction)).perform(swipeLeft());
+
+        // Final Conclusion
+        onView(withId(R.id.autoText_final_grape_variety))
+                .perform(scrollTo(), replaceText("Cabernet Sauvignon"));
+        onView(withId(R.id.autoText_final_country))
+                .perform(scrollTo(), replaceText("United States"));
+        onView(withId(R.id.autoText_final_region))
+                .perform(scrollTo(), replaceText("California"));
+        onView(withId(R.id.autoText_final_quality))
+                .perform(scrollTo(), replaceText("None"));
+        onView(withId(R.id.autoText_final_vintage))
+                .perform(scrollTo(), replaceText("2012"));
+        onView(withId(R.id.button_submit_final_conclusion))
+                .perform(click());
+
+        //TODO: Figure out why I cannot get this test to work correctly.
+        intended(
+                toPackage(VarietyResultsActivity.class.getName())
+//                hasExtras(allOf(
+//                        hasEntry(equalTo(IS_RED_WINE), equalTo(true)),
+//                        hasEntry(equalTo(USER_CONCLUSION_VARIETY), equalTo("Cabernet Sauvignon")),
+//                        hasEntry(equalTo(USER_CONCLUSION_COUNTRY), equalTo("United States")),
+//                        hasEntry(equalTo(USER_CONCLUSION_REGION), equalTo("California")),
+//                        hasEntry(equalTo(USER_CONCLUSION_QUALITY), equalTo("None")),
+//                        hasEntry(equalTo(USER_CONCLUSION_VINTAGE), equalTo(2012))))
+        );
+
+        onView(withId(R.id.textView_our_guess)).check(matches(not(isDisplayed())));
+    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//        Intents.release();
+//    }
 }
