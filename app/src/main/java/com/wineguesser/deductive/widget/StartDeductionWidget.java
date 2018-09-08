d115cba4e0af4c337eeb3c15d5c25728fdf15634
Cory <chanson5000@gmail.com>
2018-09-07 23:59:34 -0500

    Fixed bug.

--- app/src/main/java/com/wineguesser/deductive/widget/StartDeductionWidget.java
+++ app/src/main/java/com/wineguesser/deductive/widget/StartDeductionWidget.java
@@ -17,16 +17,12 @@ public class StartDeductionWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-//
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_start_deduction);
 
         views.setOnClickPendingIntent(R.id.button_red_wine, getPendingSelfIntent(context, true));
         views.setOnClickPendingIntent(R.id.button_white_wine, getPendingSelfIntent(context, false));
 
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-//
 //        // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
