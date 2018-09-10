daeec0719c765d96d335ecf508694d1e687b2d7f
Cory <chanson5000@gmail.com>
2018-09-09 20:37:57 -0500

    Removed toolbar back button.

--- app/src/main/java/com/wineguesser/deductive/view/HistoryRecordActivity.java
+++ app/src/main/java/com/wineguesser/deductive/view/HistoryRecordActivity.java
@@ -4,8 +4,6 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -31,10 +29,6 @@ public class HistoryRecordActivity extends AppCompatActivity {
                 this, R.layout.activity_history_record);
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
 
         historyRecord = ViewModelProviders.of(this)
                 .get(HistoryRecordViewModel.class);
