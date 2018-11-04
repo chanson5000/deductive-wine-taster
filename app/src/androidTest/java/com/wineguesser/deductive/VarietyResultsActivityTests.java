e79cbb16619fe5cfc14c4399fc8e1c845fc1ab8a
Cory Hanson <chanson5000@gmail.com>
2018-11-03 23:53:45 -0500

    Workstation transfer.

--- app/src/androidTest/java/com/wineguesser/deductive/VarietyResultsActivityTests.java
+++ app/src/androidTest/java/com/wineguesser/deductive/VarietyResultsActivityTests.java
@@ -20,9 +20,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-
 @RunWith(AndroidJUnit4.class)
-public class VarietyResultsActivityTests implements DeductionFormContract {
+class VarietyResultsActivityTests implements DeductionFormContract {
 
     @Rule
     public final ActivityTestRule<VarietyResultsActivity> activityTestRule =
