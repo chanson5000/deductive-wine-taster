45bf8f58d2b7dcfb1a4ae277afce35e3571ab085
Cory <chanson5000@gmail.com>
2018-11-14 20:42:37 -0600

    Workstation transfer.

---
+++ app/src/test/java/com/wineguesser/deductive/viewmodel/HistoryActivityViewModelTests.java
@@ -0,0 +1,34 @@
+package com.wineguesser.deductive.viewmodel;
+
+import com.wineguesser.deductive.repository.ConclusionsRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.doReturn;
+
+public class HistoryActivityViewModelTests {
+    private HistoryActivityViewModel historyActivityViewModel;
+
+
+    @Mock
+    private ConclusionsRepository conclusionsRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        historyActivityViewModel = new HistoryActivityViewModel();
+
+    }
+
+    @Test
+    public void getUserConclusionsTest() {
+
+        doReturn(anyObject()).when(conclusionsRepository).getConclusionsForUser("test");
+
+        System.out.println(historyActivityViewModel.getUserConclusions("test"));
+    }
+}
